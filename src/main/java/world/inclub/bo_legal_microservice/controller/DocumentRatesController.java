7e6ff72f7771cef4a8c6c4097bcea94f920852c7
Alessandro <31327229+alessandrochipulina@users.noreply.github.com>
2025-05-08 11:28:20 -0500

    Se agrega soporte para LOGS, emite formato JSON para Grafana/Loki

--- src/main/java/world/inclub/bo_legal_microservice/controller/DocumentRatesController.java
+++ src/main/java/world/inclub/bo_legal_microservice/controller/DocumentRatesController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 import world.inclub.bo_legal_microservice.domain.models.*;
 import world.inclub.bo_legal_microservice.domain.request.ApiResponse;
@@ -16,25 +17,27 @@ import world.inclub.bo_legal_microservice.infraestructure.repositories.DocumentR
 
 @RestController
 @RequestMapping("/api/v1/document/rates")
+@Slf4j
 public class DocumentRatesController {
     
     @Autowired 
     private DocumentRatesRepository drr;
 
     @GetMapping("/all")
-    @Validated
+    @Validated    
     Mono<ResponseEntity<ApiResponse<List<DocumentRates>>>> 
     getAllDocumentRates() 
     {
+        log.info("getAllDocumentRates: Retrieving all document rates");
+
         return drr.findAll()
         .collectList()
         .map(documentRates -> {
-            ApiResponse<List<DocumentRates>> response = new ApiResponse<>(
-                "200",
-                "Document rates retrieved successfully",
-                documentRates );
+            ApiResponse<List<DocumentRates>> response = new ApiResponse<>(documentRates, "Document rates retrieved successfully");
+            log.debug("getAllDocumentRates: response: {}", response.toString());
             return ResponseEntity.ok(response);
         })
-        .switchIfEmpty(Mono.error(new IllegalArgumentException("No existen tarifas de documentos")));        
+        .switchIfEmpty(Mono.error(new IllegalArgumentException("No existen tarifas de documentos")))
+        .onErrorResume(e -> Mono.error(new IllegalArgumentException(e.getMessage())));
     }
 }
