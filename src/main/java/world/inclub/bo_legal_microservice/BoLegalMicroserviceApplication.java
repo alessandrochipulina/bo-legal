1ec03e222374dc3a75db00e92d53e3a9de8f9480
Alessandro <31327229+alessandrochipulina@users.noreply.github.com>
2025-05-08 13:51:10 -0500

    Correcciones menores

--- src/main/java/world/inclub/bo_legal_microservice/BoLegalMicroserviceApplication.java
+++ src/main/java/world/inclub/bo_legal_microservice/BoLegalMicroserviceApplication.java
@@ -4,10 +4,8 @@ import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 @EnableR2dbcRepositories
 @SpringBootApplication
